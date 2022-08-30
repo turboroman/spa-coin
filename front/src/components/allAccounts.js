8ffe35bd381ee6c15b994f321ad40d1a1434395e
turboroman <roma8romenskiy@gmail.com>
2022-08-30 05:57:28 +0100

    re-push 2.0

--- front/src/components/allAccounts.js
+++ front/src/components/allAccounts.js
@@ -2,8 +2,7 @@ import { el, setChildren } from 'redom';
 import '../style.scss';
 import { createHeaderWithNav } from './header.js';
 import Choices from 'choices.js';
-import { LS, changeAddress, appWrapper } from '../index.js';
-import { initPage } from '../index.js';
+import { LS, changeAddress, appWrapper, initPage } from '../index.js';
 
 const token = JSON.parse(LS.getItem('token'));
 const accountsData = JSON.parse(LS.getItem('accounts'));
@@ -27,7 +26,6 @@ export function createAllAccountsPage() {
   setChildren(accountsSelect, [optionHeading, optionNumber, optionBalance, optionLastTrans]);
 
   accountsSelect.addEventListener('change', () => {
-    console.log(accountsData)
     if (accountsSelect.value === 'По номеру') {
       accountsData.sort((a, b) => a.account > b.account ? 1 : -1);
     }
@@ -78,87 +76,86 @@ export function createAllAccountsPage() {
 
   function createAccountsList() {
     cardsList.innerHTML = '';
-    accountsData.forEach(account => {
-      const card = el('div', { class: 'accounts__card card' }, [
-        el('h3', { class: 'card__title' }, account.account),
-        el('span', { class: 'card__balance' }, account.balance + 'P'),
-      ]);
+    if (accountsData) {
+      accountsData.forEach(account => {
+        const card = el('div', { class: 'accounts__card card' }, [
+          el('h3', { class: 'card__title' }, account.account),
+          el('span', { class: 'card__balance' }, account.balance + 'P'),
+        ]);
 
-      if (account.transactions[0]) {
-        console.log(account.transactions[0].date)
-        const dateFromData = account.transactions[0].date;
-        const year = dateFromData.split('-')[0];
-        const monthNumber = dateFromData.split('-')[1];
-        let monthWord;
-        switch (monthNumber) {
-          case '01':
-            monthWord = 'Январь';
-          case '02':
-            monthWord = 'Февраль';
-          case '03':
-            monthWord = 'Март';
-          case '04':
-            monthWord = 'Апрель';
-          case '05':
-            monthWord = 'Май';
-          case '06':
-            monthWord = 'Июнь'; 
-          case '07':
-            monthWord = 'Июль';
-          case '08':
-            monthWord = 'Август';
-          case '9':
-            monthWord = 'Сентябрь';
-          case '10':
-            monthWord = 'Октябрь';
-          case '11':
-            monthWord = 'Ноябрь';
-          case '12':
-            monthWord = 'Декабрь';
+        if (account.transactions[0]) {
+          const dateFromData = account.transactions[0].date;
+          const year = dateFromData.split('-')[0];
+          const monthNumber = dateFromData.split('-')[1];
+          let monthWord;
+          switch (monthNumber) {
+            case '01':
+              monthWord = 'Январь';
+            case '02':
+              monthWord = 'Февраль';
+            case '03':
+              monthWord = 'Март';
+            case '04':
+              monthWord = 'Апрель';
+            case '05':
+              monthWord = 'Май';
+            case '06':
+              monthWord = 'Июнь';
+            case '07':
+              monthWord = 'Июль';
+            case '08':
+              monthWord = 'Август';
+            case '9':
+              monthWord = 'Сентябрь';
+            case '10':
+              monthWord = 'Октябрь';
+            case '11':
+              monthWord = 'Ноябрь';
+            case '12':
+              monthWord = 'Декабрь';
+          }
+          const day = dateFromData.split('-')[2].split('T')[0];
+          const dateToShow = day + ' ' + monthWord + ' ' + year;
+
+          const cardBottom = el('div', { class: 'card__bottom' }, [
+            el('div', { class: 'card__transaction' }, [
+              el('span', { class: 'card__trans-text' }, 'Последняя транзакция'),
+              el('span', { class: 'card__trans-date' }, dateToShow),
+            ])
+          ]);
+          cardBottom.append(createOpenBtn());
+
+          card.append(cardBottom);
+
+        } else {
+          const cardBottom = el('div', { class: 'card__bottom' }, [
+            el('div', { class: 'card__transaction' }, [
+              el('span', { class: 'card__trans-text' }, 'Список транзакций пуст'),
+            ]),
+          ]);
+          cardBottom.append(createOpenBtn());
+
+          card.append(cardBottom);
         }
-        const day = dateFromData.split('-')[2].split('T')[0];
-        const dateToShow = day + ' ' + monthWord + ' ' + year;
 
-        const cardBottom = el('div', { class: 'card__bottom' }, [
-          el('div', { class: 'card__transaction' }, [
-            el('span', { class: 'card__trans-text' }, 'Последняя транзакция'),
-            el('span', { class: 'card__trans-date' }, dateToShow),
-          ])
-        ]);
-        cardBottom.append(createOpenBtn());
+        function createOpenBtn() {
+          const openAccountBtn = el('button', { class: 'card__btn blue-btn' }, 'Открыть');
 
-        card.append(cardBottom);
+          openAccountBtn.addEventListener('click', async (e) => {
+            e.preventDefault();
+            history.pushState(null, '', '/account');
 
-      } else {
-        const cardBottom = el('div', { class: 'card__bottom' }, [
-          el('div', { class: 'card__transaction' }, [
-            el('span', { class: 'card__trans-text' }, 'Список транзакций пуст'),
-          ]),
-        ]);
-        cardBottom.append(createOpenBtn());
+            const moduleAccount = await import('./accountPage.js');
+            appWrapper.innerHTML = '';
+            appWrapper.append(moduleAccount.createAccountPage(account));
 
-        card.append(cardBottom);
-      }
-
-      function createOpenBtn() {
-        const openAccountBtn = el('button', { class: 'card__btn blue-btn' }, 'Открыть');
-
-        openAccountBtn.addEventListener('click', async (e) => {
-          e.preventDefault();
-          history.pushState(null, '', '/account');
-
-          const moduleAccount = await import('./accountPage.js');
-          appWrapper.innerHTML = '';
-          appWrapper.append(moduleAccount.createAccountPage(account));
-
-          window.onpopstate = () => initPage();
-        });
-
-        return openAccountBtn;
-      }
-
-      cardsList.append(card);
-    })
+            window.onpopstate = () => initPage();
+          });
+          return openAccountBtn;
+        }
+        cardsList.append(card);
+      })
+    }
   }
 
   accounts.append(cardsList);
